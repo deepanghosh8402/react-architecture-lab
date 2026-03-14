94fcddd28d2a6501a7b2dfd394757edda9baa8a3
deepanghosh8402 <deepanghosh8402@gmail.com>
2026-03-13 21:13:35 -0700

    update Product Listing Page

--- src/pages/RamadaPage.jsx
+++ src/pages/RamadaPage.jsx
@@ -4,25 +4,19 @@ import { useSelector } from 'react-redux';
 
 export default function RamadaPage() {
 
-const data= useSelector(state=> state.ramdaProducts.items)
-const electronics = R.filter(
-  R.propEq("electronics", "category"),
-  data
-);
+  const data = useSelector(state => state.ramdaProducts.items)
+  const electronics = R.filter(
+    R.propEq("electronics", "category"),
+    data
+  );
   return (
-    <div> <h2>Electronics</h2>
- <h2>Electronics</h2>
-
-      {electronics.map(p => (
-        <div key={p.id}>
-          {p.name} - ${p.price}
-        </div>
-      ))}g
+    <div>
+      <h2>Electronics</h2>
       {electronics.map(p => (
         <div key={p.id}>
           {p.name} - ${p.price}
         </div>
       ))}</div>
-    
+
   )
 }
